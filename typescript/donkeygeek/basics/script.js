37158a6d8f5018fe3c7794f4486e2c4d003f1c22
Jeremy <jeremy.teurterie@gmail.com>
2023-08-09 10:18:57 +0700

    update

--- typescript/donkeygeek/basics/script.js
+++ typescript/donkeygeek/basics/script.js
@@ -1,4 +1,3 @@
-// let + const
 // const
 var pi = Math.PI;
 var API_KEY = 'iwdfnjwnwnfr';
@@ -7,3 +6,29 @@ console.log(pi);
 var number = 20;
 number = 21;
 console.log(number);
+// type inference
+var firstName;
+firstName = true;
+firstName = 'red';
+firstName = 10;
+// string
+var lastName = 'Dupont';
+// number
+var age = 20;
+// boolean
+var isLogging = true;
+lastName = 'Dora';
+age = 25;
+isLogging = false;
+lastName = 23;
+age = true;
+isLogging = 'Hello';
+// type assignation
+var alien = 'Roger';
+var width;
+width = 20;
+width = true;
+var sum = function (val1, val2) {
+    return val1 + val2;
+};
+sum(20, 15);
--- typescript/donkeygeek/basics/script.ts
+++ typescript/donkeygeek/basics/script.ts
@@ -1,5 +1,3 @@
-// let + const
-
 // const
 const pi = Math.PI;
 const API_KEY = 'iwdfnjwnwnfr';
@@ -9,3 +7,36 @@ console.log(pi);
 let number = 20;
 number = 21;
 console.log(number);
+
+// type inference
+let firstName;
+firstName = true;
+firstName = 'red';
+firstName = 10;
+
+// string
+let lastName = 'Dupont';
+// number
+let age = 20;
+// boolean
+let isLogging = true;
+
+lastName = 'Dora';
+age = 25;
+isLogging = false;
+
+lastName = 23;
+age = true;
+isLogging = 'Hello';
+
+// type assignation
+let alien: string = 'Roger';
+let width: number;
+width = 20;
+width = true;
+
+const sum = (val1: number, val2: number) => {
+  return val1 + val2;
+};
+
+sum(20, 15);
