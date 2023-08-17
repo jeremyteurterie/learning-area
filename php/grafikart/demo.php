a79b6e76e17c69cce4b0875833c88c2210a6d244
Jeremy <jeremy.teurterie@gmail.com>
2023-08-17 09:23:24 +0700

    update

---
+++ php/grafikart/demo.php
@@ -0,0 +1,7 @@
+<?php
+$first_name = 'Marc';
+$last_name = 'Doe';
+$note1 = 10;
+$note2 = 20;
+echo 'Bonjour ' . $first_name . ' ' . $last_name . ' vous avez eu ' . (($note1 + $note2) / 2) . ' de moyenne';
+?>
